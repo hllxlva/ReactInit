8ef1b3929ac0d48b6041b9afef3b16f79e4ef6e0
東條　翔 <xt@MacBook-Air-Xt.local>
2019-09-25 23:57:31 +0900

    style add

--- main.jsx
+++ main.jsx
@@ -1,10 +1,20 @@
 function App() {
     return (
         <div>
-            <Tweet content="Hello" />
-            <Tweet content="React!" />
-        </div>
-    )
+      <Tweet
+        icon=""
+        displayName="もろこし太郎"
+        accountName="morokoshi"
+        content="今日も1日もろこしがうまい"
+      />
+      <Tweet
+        icon=""
+        displayName="エビデンス"
+        accountName="evidence"
+        content="かにみそたべたい"
+      />
+    </div>
+    );
   }
   
   const target = document.querySelector('#app');
