1fd77efcdf8ed9c3b466cb3b2d3d33e389a64ed8
東條　翔 <xt@MacBook-Air-Xt.local>
2019-10-07 22:04:55 +0900

    add Timeline

--- main.jsx
+++ main.jsx
@@ -1,21 +1,27 @@
 function App() {
-    return (
-        <div>
-      <Tweet
-        icon=""
-        displayName="もろこし太郎"
-        accountName="morokoshi"
-        content="今日も1日もろこしがうまい"
-      />
-      <Tweet
-        icon=""
-        displayName="エビデンス"
-        accountName="evidence"
-        content="かにみそたべたい"
-      />
+  const tweets = [
+    {
+      id: 0,
+      icon: '',
+      displayName: 'もろこし太郎',
+      accountName: 'morokoshi',
+      content: '今日も1日もろこしがうまい'
+    },
+    {
+      id: 1,
+      icon: '',
+      displayName: 'エビデンス',
+      accountName: 'evidence',
+      content: 'かにみそたべたい'
+    }
+  ];
+
+  return (
+    <div>
+      <Timeline tweets={tweets}/>
     </div>
-    );
-  }
-  
-  const target = document.querySelector('#app');
-  ReactDOM.render(<App/>, target);
+  );
+}
+
+const target = document.querySelector('#app');
+ReactDOM.render(<App/>, target);
